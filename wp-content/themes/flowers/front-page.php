dfd64d5c1e0769e835763fa304f2ae6ba1bfce65
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-04-06 21:57:57 +0300

    change

--- wp-content/themes/flowers/front-page.php
+++ wp-content/themes/flowers/front-page.php
@@ -4,7 +4,7 @@
         <div class="slider-container">
             <div class="slider-banner">                
                 <div class="swiper-slide">
-                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/Banner/Compresed/1.jpg" class='bg'>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/banner1.jpeg" class='bg'>
                 </div>
                 <div class="swiper-slide">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/img/Banner/Compresed/2.jpg" class='bg'>
