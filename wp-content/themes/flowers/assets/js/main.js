5f92f323bbce50560fc3ff097e3cade39f0ddc23
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-04-07 12:03:41 +0300

    change

--- wp-content/themes/flowers/assets/js/main.js
+++ wp-content/themes/flowers/assets/js/main.js
@@ -1,135 +1,32 @@
 const forms = document.querySelectorAll("#contact-us  form");
 if (forms.length) {
-    forms.forEach((form) => {
-        form.addEventListener("submit", async(e) => {
-            //   e.preventDefault();
-            const formData = new FormData(form);
+  forms.forEach((form) => {
+    form.addEventListener("submit", async (e) => {
+      //   e.preventDefault();
+      const formData = new FormData(form);
 
-            const res = await fetch(form.attributes.action.value, {
-                method: form.attributes.method.value,
-                body: formData,
-            }).then((res) => res.json());
+      const res = await fetch(form.attributes.action.value, {
+        method: form.attributes.method.value,
+        body: formData,
+      }).then((res) => res.json());
 
-            //запит розкоментувати і рес = тру видалили, має працювати на серваку
+      //запит розкоментувати і рес = тру видалили, має працювати на серваку
 
-            //   const res = true;
-        });
+      //   const res = true;
     });
+  });
 }
 const anchors = document.querySelectorAll('a[href*="#"]');
 for (let anchor of anchors) {
-    anchor.addEventListener("click", function(event) {
-        event.preventDefault();
-        const blockID = anchor.getAttribute("href");
-        document.querySelector("" + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: "center",
-        });
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "center",
     });
+  });
 }
-<<<<<<< HEAD
-$(document).ready(function() {
-    $(".header-burger").click(function(event) {
-        $(".header-nav").toggleClass("active");
-        $(".header-burger").toggleClass("active");
-    });
-    $(".guestion-item").click(function(event) {
-        // $(".answer").toggleClass("open");
-        $(this).closest("li").find(".answer").toggleClass("open");
-        $(this).closest("li").find("img").toggleClass("open");
-    });
-    $(".description-top").click(function(event) {
-        $(".description-body").toggleClass("open");
-        $(".description-top").find("img").toggleClass("open");
-    });
-    $(".category").click(function(event) {
-        $(".sort-list").removeClass("active");
-        $(".sort").closest("div").find("img").removeClass("active");
-        $(".category-list").toggleClass("active");
-        $(this).closest("div").find("img").toggleClass("active");
-    });
-    $(".sort").click(function(event) {
-        $(".category-list").removeClass("active");
-        $(".category").closest("div").find("img").removeClass("active");
-        $(".sort-list").toggleClass("active");
-        $(this).closest("div").find("img").toggleClass("active");
-    });
-    $(".options-btn").click(function(event) {
-        $(this).closest("tr").find(".variation-list").toggleClass("active");
-        $(this).find("img").toggleClass("active");
-    });
-    $(".variation-item").click(function(event) {
-        $(this).closest("ul").find(".variation-item").removeClass("select");
-        // $(".variation-item").removeClass("select");
-        $(this).toggleClass("select");
-        $(".options-btn span").text($(".select").text());
-    });
-    $(document).on("click", ".step-body .btn", function(event) {
-        $(this)
-            .closest("li")
-            .next("li")
-            .find(".step-body")
-            .removeClass("hidden-step");
-    });
-    const radiobuttonsIni = () => {
-        $.each($(".radiobuttons-item"), function(index, val) {
-            if (
-                $(this).find("input[type=radio]").prop("checked") == true ||
-                $(this).find("input[type=hidden]").length
-            ) {
-                $(this).addClass("check");
-            }
-        });
-    };
-    radiobuttonsIni();
-    $(document).on("DOMSubtreeModified", radiobuttonsIni);
-    $(document).on("click", ".radiobuttons-item", function(event) {
-        $(this)
-            .parents("#shipping_method")
-            .find(".radiobuttons-item")
-            .removeClass("check");
-        $(this)
-            .parents("#shipping_method")
-            .find(".radiobuttons-item input[type=radio]")
-            .prop("checked", false);
-        $(this).toggleClass("check");
-        $(this).find("input[type=radio]").prop("checked", true);
-        $(this).find("input[type=radio]").trigger("change");
-        return false;
-    });
-    // change quantity
-    $(document).on("click", ".change-quantity", function(e) {
-        let currentItem = $(this).closest(".quantity");
-        let quantityInput = currentItem.find(".input-quantity");
-        let quantity = parseInt(quantityInput.val());
-        let newQuantity = 0;
-        if ($(this).hasClass("plus")) {
-            newQuantity = quantity + 1;
-        }
-        if ($(this).hasClass("minus")) {
-            newQuantity = quantity == 0 ? quantity : quantity - 1;
-        }
-        quantityInput.attr("value", newQuantity);
-        currentItem.find(".amount").text(newQuantity);
-        if ($("[name=update_cart]").length) {
-            $("[name=update_cart]").prop({ disabled: false, "aria-disabled": false });
-            $("[name=update_cart]").trigger("click");
-        }
-    });
-    // choose variation
-    $(".variations_form .variation-item").on("click", function(e) {
-        let variatName = $(this).closest(".variation-list").attr("data-name");
-        let variatSelect = $(`select[name=${variatName}]`);
-        variatSelect.find("option").removeProp("selected");
-        variatSelect
-            .find(`option[value='${$(this).attr("data-value")}']`)
-            .prop("selected", "selected");
-        variatSelect.trigger("change");
-    });
-    // update checkout
-    $(document).on("change", "input[name=billing_postcode]", function(e) {
-        $(document.body).trigger("update_checkout");
-=======
 $(document).ready(function () {
   $(".header-burger").click(function (event) {
     $(".header-nav").toggleClass("active");
@@ -162,7 +59,7 @@ $(document).ready(function () {
   });
   $(".variation-item").click(function (event) {
     $(this).closest("ul").find(".variation-item").removeClass("select");
-    $(".variation-list").removeClass("active");
+    // $(".variation-item").removeClass("select");
     $(this).toggleClass("select");
     $(".options-btn span").text($(".select").text());
   });
@@ -181,8 +78,57 @@ $(document).ready(function () {
       ) {
         $(this).addClass("check");
       }
->>>>>>> 9a4f692 (change)
     });
+  };
+  radiobuttonsIni();
+  $(document).on("DOMSubtreeModified", radiobuttonsIni);
+  $(document).on("click", ".radiobuttons-item", function (event) {
+    $(this)
+      .parents("#shipping_method")
+      .find(".radiobuttons-item")
+      .removeClass("check");
+    $(this)
+      .parents("#shipping_method")
+      .find(".radiobuttons-item input[type=radio]")
+      .prop("checked", false);
+    $(this).toggleClass("check");
+    $(this).find("input[type=radio]").prop("checked", true);
+    $(this).find("input[type=radio]").trigger("change");
+    return false;
+  });
+  // change quantity
+  $(document).on("click", ".change-quantity", function (e) {
+    let currentItem = $(this).closest(".quantity");
+    let quantityInput = currentItem.find(".input-quantity");
+    let quantity = parseInt(quantityInput.val());
+    let newQuantity = 0;
+    if ($(this).hasClass("plus")) {
+      newQuantity = quantity + 1;
+    }
+    if ($(this).hasClass("minus")) {
+      newQuantity = quantity == 0 ? quantity : quantity - 1;
+    }
+    quantityInput.attr("value", newQuantity);
+    currentItem.find(".amount").text(newQuantity);
+    if ($("[name=update_cart]").length) {
+      $("[name=update_cart]").prop({ disabled: false, "aria-disabled": false });
+      $("[name=update_cart]").trigger("click");
+    }
+  });
+  // choose variation
+  $(".variations_form .variation-item").on("click", function (e) {
+    let variatName = $(this).closest(".variation-list").attr("data-name");
+    let variatSelect = $(`select[name=${variatName}]`);
+    variatSelect.find("option").removeProp("selected");
+    variatSelect
+      .find(`option[value='${$(this).attr("data-value")}']`)
+      .prop("selected", "selected");
+    variatSelect.trigger("change");
+  });
+  // update checkout
+  $(document).on("change", "input[name=billing_postcode]", function (e) {
+    $(document.body).trigger("update_checkout");
+  });
 });
 
 // const filterSlide = document.querySelectorAll(".swiper-slide");
