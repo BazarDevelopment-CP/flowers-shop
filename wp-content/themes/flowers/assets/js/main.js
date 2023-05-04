575b69e6352b01da970209a77c37143b4ace0b60
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-05-04 14:21:56 +0300

    change

--- wp-content/themes/flowers/assets/js/main.js
+++ wp-content/themes/flowers/assets/js/main.js
@@ -61,7 +61,9 @@ $(document).ready(function () {
     $(this).closest("ul").find(".variation-item").removeClass("select");
     // $(".variation-item").removeClass("select");
     $(".variation-list").removeClass("active");
+    $(this).find("img").toggleClass("active");
     $(this).toggleClass("select");
+
     $(".options-btn").text($(".select").text());
   });
   $(document).on("click", ".step-body .btn", function (event) {
